fe7c5f1e2775f786bd876ac22d54523ecb345548
Rishabh Rao <rishabhsrao@gmail.com>
2014-12-24 09:20:27 +0530

    Initialized project with IntelliJ IDEA.

---
+++ app/src/main/java/com/rishabhsrao/similarartistfinder/SearchActivity.java
@@ -0,0 +1,35 @@
+package com.rishabhsrao.similarartistfinder;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class SearchActivity extends Activity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_search);
+  }
+
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.menu_search, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    int id = item.getItemId();
+
+    //noinspection SimplifiableIfStatement
+    if (id == R.id.action_settings) {
+      return true;
+    }
+
+    return super.onOptionsItemSelected(item);
+  }
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+# SimilarArtistFinder
+
+> An Android app to search for artists who are similar to your favourite artist.
