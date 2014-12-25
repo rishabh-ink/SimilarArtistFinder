7e9cf3bbe0cddd293632cac741f94a74167c8f2f
Rishabh Rao <rishabhsrao@gmail.com>
2014-12-25 19:15:05 +0530

    Use Jackson to parse the artist info JSON.

--- app/src/main/java/com/rishabhsrao/similarartistfinder/controllers/similarartists/RetrieveSimilarArtistsAsyncTask.java
+++ app/src/main/java/com/rishabhsrao/similarartistfinder/controllers/similarartists/RetrieveSimilarArtistsAsyncTask.java
@@ -3,68 +3,49 @@ package com.rishabhsrao.similarartistfinder.controllers.similarartists;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.util.Log;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rishabhsrao.similarartistfinder.settings.Settings;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.io.BufferedInputStream;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-public class RetrieveSimilarArtistsAsyncTask extends AsyncTask<String, Integer, JSONObject> {
+public class RetrieveSimilarArtistsAsyncTask extends AsyncTask<String, Integer, JsonNode> {
   @Override
-  protected JSONObject doInBackground(String... artistName) {
+  protected JsonNode doInBackground(String... artistName) {
     Uri.Builder uri = Settings.API_URI.appendQueryParameter("artistName", artistName[0]);
     Log.d(RetrieveSimilarArtistsAsyncTask.class.getSimpleName(), "Built URL: " + uri.toString());
 
-    JSONObject inputJsonObject = null;
+    JsonNode jsonNode = null;
     HttpURLConnection urlConnection = null;
 
     try {
       URL url = new URL(uri.toString());
       urlConnection = (HttpURLConnection) url.openConnection();
+
       BufferedInputStream bufferedInputStream = new BufferedInputStream(urlConnection.getInputStream());
 
-      String inputString = this.readStream(bufferedInputStream);
-
-      Log.d(RetrieveSimilarArtistsAsyncTask.class.getSimpleName(), "Obtained stream: " + inputString);
-      inputJsonObject = new JSONObject(inputString);
+      ObjectMapper objectMapper = new ObjectMapper();
+      jsonNode = objectMapper.readTree(bufferedInputStream);
     } catch (MalformedURLException e) {
       e.printStackTrace();
     }
     catch (IOException e) {
       e.printStackTrace();
-    }
-    catch (JSONException e) {
-      e.printStackTrace();
-    }
-    finally {
+    } finally {
       Log.e(RetrieveSimilarArtistsAsyncTask.class.getSimpleName(), "Disconnecting...");
       urlConnection.disconnect();
     }
 
-    return inputJsonObject;
+    return jsonNode;
   }
 
   @Override
-  protected void onPostExecute(JSONObject jsonObject) {
-    super.onPostExecute(jsonObject);
-    Log.e(RetrieveSimilarArtistsAsyncTask.class.getSimpleName(), "Retrieved JSON: " + jsonObject.toString());
-  }
-
-  private String readStream(BufferedInputStream bufferedInputStream) throws IOException {
-    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(bufferedInputStream));
-    String line;
-    StringBuilder stringBuilder = new StringBuilder();
-
-    while(null != (line = bufferedReader.readLine())) {
-      stringBuilder.append(line);
-    }
-
-    return stringBuilder.toString();
+  protected void onPostExecute(JsonNode jsonNode) {
+    super.onPostExecute(jsonNode);
+    Log.e(RetrieveSimilarArtistsAsyncTask.class.getSimpleName(), "Retrieved JSON: " + jsonNode.toString());
   }
 }
--- app/src/main/java/com/rishabhsrao/similarartistfinder/controllers/similarartists/SimilarArtistsActivity.java
+++ app/src/main/java/com/rishabhsrao/similarartistfinder/controllers/similarartists/SimilarArtistsActivity.java
@@ -7,16 +7,18 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.rishabhsrao.similarartistfinder.R;
+import com.rishabhsrao.similarartistfinder.models.Artist;
 import com.rishabhsrao.similarartistfinder.settings.Settings;
-import org.json.JSONObject;
 
+import java.io.IOException;
+import java.util.Iterator;
 import java.util.concurrent.ExecutionException;
 
 public class SimilarArtistsActivity extends Activity {
-  private String artistName;
-
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -25,19 +27,7 @@ public class SimilarArtistsActivity extends Activity {
     ActionBar actionBar = getActionBar();
     actionBar.setDisplayHomeAsUpEnabled(true);
 
-    Intent searchArtistIntent = this.getIntent();
-    this.artistName = searchArtistIntent.getStringExtra(Settings.INTENT_EXTRA_ARTIST_NAME);
-
-    Log.d(SimilarArtistsActivity.class.getName(), "Searching for " + this.artistName);
-    try {
-      JSONObject artistInfo = new RetrieveSimilarArtistsAsyncTask().execute(this.artistName).get();
-      TextView artistBio = (TextView) this.findViewById(R.id.activity_similar_artists_artistbio);
-
-    } catch (InterruptedException e) {
-      e.printStackTrace();
-    } catch (ExecutionException e) {
-      e.printStackTrace();
-    }
+    this.fetchArtistInfo();
   }
 
   @Override
@@ -64,5 +54,36 @@ public class SimilarArtistsActivity extends Activity {
     }
 
     return super.onOptionsItemSelected(item);
+  }
+
+  public void fetchArtistInfo() {
+    Intent searchArtistIntent = this.getIntent();
+    String artistName = searchArtistIntent.getStringExtra(Settings.INTENT_EXTRA_ARTIST_NAME);
+    JsonNode artistInfo;
+
+    Log.d(SimilarArtistsActivity.class.getName(), "Searching for " + artistName);
+    try {
+      artistInfo = new RetrieveSimilarArtistsAsyncTask().execute(artistName).get();
+      this.parseArtistInfo(artistInfo);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    } catch (ExecutionException e) {
+      e.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void parseArtistInfo(JsonNode artistInfo) throws IOException {
+    ObjectMapper objectMapper = new ObjectMapper();
+    ArrayNode items = (ArrayNode) artistInfo.path("value").path("items").path(0).path("artist").path("similar").path("artist");
+
+    Iterator<JsonNode> jsonNodeIterator = items.elements();
+
+    while (jsonNodeIterator.hasNext()) {
+      Artist artist = objectMapper.readValue(jsonNodeIterator.next().traverse(), Artist.class);
+
+      Log.d(SimilarArtistsActivity.class.getSimpleName(), "Similar artist: " + artist);
+    }
   }
 }
